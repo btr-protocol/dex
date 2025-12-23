d3025658eb49cb859634b0ac4b540bb8f2fb5b5a
pde-rent <paul.ed.de.renty@gmail.com>
2025-12-23 17:56:52 +0700

    [refac] Add constants barrel export
    Centralize all constant exports in single index file.

---
+++ front/src/constants/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Constants - centralized exports
+ */
+
+export * from './icons';
+export * from './design';
+export * from './navigation';
+export * from './contracts';
+export * from './tokens';
